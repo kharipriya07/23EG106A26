e30f6bc6a58b68b450e462127b02366628114e17
kharipriya07 <karnamharipriya11@gmail.com>
2025-08-23 11:44:49 +0530

    Fix:Corrected console.log syntax

--- Airport/app.js
+++ Airport/app.js
@@ -42,5 +42,5 @@ module.exports = app;
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(Server is running on port ${port});
+  console.log(`Server is running on port ${port}`);
 });
